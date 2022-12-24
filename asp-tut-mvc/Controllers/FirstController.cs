5826961152f661421898320661f697fdafa1845f
lis-dima <lisiev.dmytro@gmail.com>
2022-12-24 18:04:30 +0000

    short MVC prev

---
+++ asp-tut-mvc/Controllers/FirstController.cs
@@ -0,0 +1,31 @@
+﻿using asp_tut_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp_tut_mvc.Controllers
+{
+    public class FirstController : Controller
+    {
+        static List<DogViewModel> DogViewModels { get; set; }
+        public IActionResult Index()
+        {
+            if (DogViewModels == null) DogViewModels = new List<DogViewModel>();
+            return View(DogViewModels);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            DogViewModels.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public string Str()
+        {
+            return "some str";
+        }
+    }
+}
---
+++ asp-tut-mvc/Models/DogViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace asp_tut_mvc.Models
+{
+    public class DogViewModel
+    {
+        public string Name { get; set; } = "def";
+        public int Age { get; set; } = -1;
+    }
+}
